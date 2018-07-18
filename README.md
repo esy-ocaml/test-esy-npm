3defe6295e5b289fd9698887b6dca2b2f756da8a
Andrey Popp <8mayday@gmail.com>
2018-07-18 15:08:47 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# test-esy-npm
+
+[![Build Status](https://travis-ci.com/esy-ocaml/test-esy-npm.svg?branch=master)](https://travis-ci.com/esy-ocaml/test-esy-npm)
+
+This is an integration test suite for `esy install` (of [esy][]) which consist
+of popular npm packages.
+
+[esy]: https://esy.sh
---
+++ test.js
@@ -0,0 +1,89 @@
+const cases = [
+  {
+    name: "react",
+    test: `
+      require('react');
+    `
+  },
+  {
+    name: "bs-platform"
+  },
+  {
+    name: "browserify",
+    test: `
+      require('browserify');
+    `
+  },
+  {
+    name: "webpack",
+    test: `
+      require('webpack');
+    `
+  },
+];
+
+const child_process = require('child_process');
+const fs = require('fs');
+const path = require('path');
+const rmSync = require('rimraf').sync;
+
+const ESYI = process.env.ESYI || 'esyi';
+const ESY = process.env.ESY || 'esy';
+
+child_process.execSync(`which ${ESYI}`, {stdio: 'inherit'});
+child_process.execSync(`which ${ESY}`, {stdio: 'inherit'});
+
+const cwd = __dirname;
+
+rmSync(path.join(cwd, '_build'));
+fs.mkdirSync(path.join(cwd, '_build'));
+
+let reposUpdated = false;
+
+for (let c of cases) {
+
+  console.log(`*** Testing ${c.name}`);
+
+  const sandboxPath = path.join(cwd, '_build', c.name);
+
+  const packageJson = {
+    name: `test-${c.name}`,
+    version: '0.0.0',
+    esy: {build: ['true']},
+    dependencies: {
+      [c.name]: "*"
+    }
+  };
+
+  fs.mkdirSync(sandboxPath);
+  fs.writeFileSync(
+    path.join(sandboxPath, 'package.json'),
+    JSON.stringify(packageJson, null, 2)
+  );
+
+  child_process.execSync(`${ESY} install`, {
+    cwd: sandboxPath,
+    stdio: 'inherit',
+  });
+
+  child_process.execSync(`${ESY} build`, {
+    cwd: sandboxPath,
+    stdio: 'inherit',
+  });
+
+  if (c.test != null) {
+    fs.writeFileSync(
+      path.join(sandboxPath, 'test.js'),
+      c.test
+    );
+  console.log(`*** Running test ***`);
+    child_process.execSync(`node ./test.js`, {
+      cwd: sandboxPath,
+      stdio: 'inherit',
+    });
+  }
+
+  console.log(`*** OK ***`);
+  rmSync(sandboxPath);
+
+}
